fe2298634ee869868f40681d848d92e8f0177ea5
LVQA1998 <quanganlevan@gmail.com>
2020-06-24 16:32:36 +0700

    cancel job

--- app/controllers/user_applies_controller.rb
+++ app/controllers/user_applies_controller.rb
@@ -1,16 +1,27 @@
 class UserAppliesController < ApplicationController
-  before_action :logged_in_user, only: :create   
+  before_action :logged_in_user, :load_post, only: %i(create destroy)   
 
   def create
-    status = current_user.user_applies.create apply_params
+    current_user.user_applies.create apply_params
     respond_to :js
   end
 
-  def destroy; end
+  def destroy
+    return if current_user.user_applies.find_by(post_id: params[:id]).delete
+    
+    flash[:danger] = t "error.error"
+    redirect_to "post/show"
+    
+    respond_to :js
+  end
 
   private 
 
   def apply_params
     params.permit UserApply::USER_APPLY_PERMIT
+  end
+
+  def load_post
+    @post = Post.find_by(id: params[:id] || params[:post_id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,17 @@ module ApplicationHelper
     profile.public_cv? ? t("public") : t("private") 
   end
 
+  def profile_avatar profile
+    return if profile.image
+
+    gravatar_image_tag(current_user.email.gsub("spam", "mdeering"))
+  end
+  
   def user_apply? post
     current_user.user_applies.post_applies(post).present?
+  end
+
+  def qualification_time qualification
+    [I18n.l(qualification.start_time), I18n.l(qualification.end_time)].join(" - ")
   end
 end
